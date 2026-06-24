1e5a595ac1380e7eeacefcc9106195c60b6a9832
Andy0703-E <dadung2707@gmail.com>
2026-06-24 22:20:37 +0800

    Fix: set default values for uptime/battery/temperature/memory in getServerInfo

--- includes/helpers.php
+++ includes/helpers.php
@@ -58,40 +58,40 @@ function getServerInfo() {
     $info = [];
     
     $info['php_version'] = phpversion();
+    $info['uptime'] = 'N/A';
+    $info['battery'] = 'N/A';
+    $info['temperature'] = 'N/A';
+    $info['mem_total'] = 0;
+    $info['mem_used'] = 0;
+    $info['mem_free'] = 0;
     
     $info['disk_total'] = @disk_total_space(SERVER_PATH) ?: 0;
     $info['disk_free'] = @disk_free_space(SERVER_PATH) ?: 0;
     $info['disk_used'] = $info['disk_total'] - $info['disk_free'];
     
-    if (PHP_OS_FAMILY === 'Linux') {
-        $mem = @file_get_contents('/proc/meminfo');
-        if ($mem) {
-            preg_match('/MemTotal:\s+(\d+)/', $mem, $m);
-            $info['mem_total'] = ($m[1] ?? 0) * 1024;
-            preg_match('/MemAvailable:\s+(\d+)/', $mem, $m);
-            $info['mem_free'] = ($m[1] ?? 0) * 1024;
-            $info['mem_used'] = $info['mem_total'] - $info['mem_free'];
-        }
-        
-        $up = @file_get_contents('/proc/uptime');
-        if ($up) {
-            $sec = (float)explode(' ', $up)[0];
-            $d = floor($sec / 86400);
-            $h = floor(($sec % 86400) / 3600);
-            $m = floor(($sec % 3600) / 60);
-            $info['uptime'] = "{$d}d {$h}h {$m}m";
-        }
-        
-        $bat = @file_get_contents('/sys/class/power_supply/battery/capacity');
-        $info['battery'] = $bat !== false ? trim($bat) . '%' : 'N/A';
-        
-        $temp = @file_get_contents('/sys/class/thermal/thermal_zone0/temp');
-        $info['temperature'] = $temp !== false ? round((int)trim($temp) / 1000, 1) . '°C' : 'N/A';
-    } else {
-        $info['uptime'] = 'N/A';
-        $info['battery'] = 'N/A';
-        $info['temperature'] = 'N/A';
+    $mem = @file_get_contents('/proc/meminfo');
+    if ($mem) {
+        preg_match('/MemTotal:\s+(\d+)/', $mem, $m);
+        $info['mem_total'] = ($m[1] ?? 0) * 1024;
+        preg_match('/MemAvailable:\s+(\d+)/', $mem, $m);
+        $info['mem_free'] = ($m[1] ?? 0) * 1024;
+        $info['mem_used'] = $info['mem_total'] - $info['mem_free'];
     }
+    
+    $up = @file_get_contents('/proc/uptime');
+    if ($up) {
+        $sec = (float)explode(' ', $up)[0];
+        $d = floor($sec / 86400);
+        $h = floor(($sec % 86400) / 3600);
+        $m = floor(($sec % 3600) / 60);
+        $info['uptime'] = "{$d}d {$h}h {$m}m";
+    }
+    
+    $bat = @file_get_contents('/sys/class/power_supply/battery/capacity');
+    if ($bat !== false) $info['battery'] = trim($bat) . '%';
+    
+    $temp = @file_get_contents('/sys/class/thermal/thermal_zone0/temp');
+    if ($temp !== false) $info['temperature'] = round((int)trim($temp) / 1000, 1) . '°C';
     
     return $info;
 }
