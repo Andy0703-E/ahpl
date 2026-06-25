41361531762e4b1602c834154267d1e2fcff8222
Andy0703-E <dadung2707@gmail.com>
2026-06-25 21:24:31 +0800

    Add ?debug param to tunnel-url.php for diagnostics

--- panel/api/tunnel-url.php
+++ panel/api/tunnel-url.php
@@ -6,13 +6,21 @@ header('Content-Type: application/json');
 
 if (!isLoggedIn()) jsonResponse(['error' => 'Unauthorized'], 401);
 
-$logFile = TUNNEL_LOG;
 $url = '';
-
-if (file_exists($logFile)) {
-    $content = file_get_contents($logFile);
+if (file_exists(TUNNEL_LOG)) {
+    $content = file_get_contents(TUNNEL_LOG);
     if (preg_match('/https:\/\/[a-zA-Z0-9-]+\.trycloudflare\.com/', $content, $m)) {
         $url = $m[0];
+    }
+    // Debug: jika ada param debug, kembalikan log mentah
+    if (isset($_GET['debug'])) {
+        jsonResponse(['url' => $url, 'log_path' => TUNNEL_LOG, 'log_exists' => true, 'log_size' => strlen($content), 'log_snippet' => substr($content, 0, 2000)]);
+        exit;
+    }
+} else {
+    if (isset($_GET['debug'])) {
+        jsonResponse(['url' => $url, 'log_path' => TUNNEL_LOG, 'log_exists' => false]);
+        exit;
     }
 }
 
